6facdc96687f47f133019940829c18568a5b8e4b
Suroboros <caixiao.stu@gmail.com>
2023-11-02 01:55:09 +0900

    matrix code

--- Util/MathUtil.h
+++ Util/MathUtil.h
@@ -8,11 +8,24 @@
 using namespace DirectX;
 
 // Override << operator, output XMVECTOR
-std::ostream& XM_CALLCONV operator<<(std::ostream& os, DirectX::FXMVECTOR vector)
+std::ostream& XM_CALLCONV operator<<(std::ostream& os, FXMVECTOR vector)
 {
-	XMFLOAT3 dest;
-	XMStoreFloat3(&dest, vector);
-	os << "(" << dest.x << ", " << dest.y << ", " << dest.z << ")";
+	XMFLOAT4 dest;
+	XMStoreFloat4(&dest, vector);
+	os << "(" << dest.x << ", " << dest.y << ", " << dest.z << ", " << dest.w << ")";
+	return os;
+}
+
+// Override << operator, output XMMATRIX
+std::ostream& XM_CALLCONV operator<<(std::ostream& os, FXMMATRIX matrix)
+{
+	for (int i = 0; i < 4; ++i)
+	{
+		os << XMVectorGetX(matrix.r[i]) << "\t"
+			<< XMVectorGetY(matrix.r[i]) << "\t"
+			<< XMVectorGetZ(matrix.r[i]) << "\t"
+			<< XMVectorGetW(matrix.r[i]) << std::endl;
+	}
 	return os;
 }
 

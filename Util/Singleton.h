001af05dbb333fe0ce583e4a748510e01617a080
Suroboros <caixiao.stu@gmail.com>
2023-11-26 05:03:41 +0900

    Update Singleton.h
    refactor and reformat

--- Util/Singleton.h
+++ Util/Singleton.h
@@ -4,46 +4,46 @@
 #include <memory>
 #include <mutex>
 
-template <class T, typename... Args>
-class Singleton
-{
-private:
-    inline static std::unique_ptr<T> instance;
-    inline static std::once_flag initializedFlag;
+template <class T>
+class Singleton {
+ private:
+  static std::unique_ptr<T> instance;
+  static std::once_flag initializedFlag;
 
-private:
-    inline void CreateInstance(Args&&... args)
-    {
-        instance.reset(new T(std::forward<Args>(args)...));
-    }
+ private:
+  inline static T& GetReference(const std::unique_ptr<T>& instancePtr) {
+    return *instancePtr;
+  }
 
-    inline static T& GetReference(const std::unique_ptr<T>& instancePtr)
-    {
-        return *instancePtr;
-    }
+  Singleton(const Singleton&) = delete;
+  Singleton(Singleton&&) = delete;
+  Singleton& operator=(const Singleton&) = delete;
+  Singleton& operator=(Singleton&&) = delete;
 
-    Singleton(const Singleton&) = delete;
-    Singleton(Singleton&&) = delete;
-    Singleton& operator=(const Singleton&) = delete;
-    Singleton& operator=(Singleton&&) = delete;
+ protected:
+  Singleton() = default;
+  virtual ~Singleton() = default;
 
-protected:
-    Singleton() = default;
-    virtual ~Singleton() = default;
+ public:
+  static T& GetInstance() {
+    std::call_once(initializedFlag, [&]() { instance.reset(new T()); });
+    return GetReference(instance);
+  }
 
-public:
-    template <typename... InstanceArgs>
-    static T& GetInstance(InstanceArgs&&... args)
-    {
-        std::call_once(initializedFlag, &Singleton::CreateInstance, std::forward<InstanceArgs>(args)...);
-        return GetReference(instance);
-    }
+  template <typename... InstanceArgs>
+  static T& GetInstance(InstanceArgs&&... args) {
+    std::call_once(
+        initializedFlag,
+        [&]() { instance.reset(new T(std::forward<InstanceArgs>(args)...)); },
+        std::forward<InstanceArgs>(args)...);
+    return GetReference(instance);
+  }
 };
 
-template <class T, typename... Args>
-inline std::unique_ptr<T> Singleton<T, Args...>::instance;
+template <class T>
+std::unique_ptr<T> Singleton<T>::instance;
 
-template <class T, typename... Args>
-inline std::once_flag Singleton<T, Args...>::initializedFlag;
+template <class T>
+std::once_flag Singleton<T>::initializedFlag;
 
-#endif // !_SINGLETON_H_
+#endif  // !_SINGLETON_H_
